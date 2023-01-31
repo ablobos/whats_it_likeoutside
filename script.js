482d1cca2efea6c1359a3170ef93b77e88a4d3a7
Abdiel "A.B." Villalobos <ablobosportfolio@gmail.com>
2023-01-30 21:58:28 -0500

    modified js file for functionality

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const temp = document.getElementById("temp"),
     date = document.getElementById("date-time"),
     currentLocation = document.getElementById("location"),
-    condition = document.getElementById("condition"),
+    condition = document.getElementById("conditions"),
     rain = document.getElementById("rain"),
     mainIcon = document.getElementById("icon"),
     uvIndex = document.querySelector(".uv-index"),
     uvText = document.querySelector(".uv-text"),
     windSpeed = document.querySelector(".wind-speed"),
     sunRise = document.querySelector(".sunrise"),
-    sunSet = document.querySelector(".sun-set"),
+    sunSet = document.querySelector(".sunset"),
     humidity = document.querySelector(".humidity"),
     visibility = document.querySelector(".visibility"),
     humidityStatus = document.querySelector(".humidity-status"),
@@ -17,13 +17,13 @@ const temp = document.getElementById("temp"),
     visibilityStatus = document.querySelector(".visibility-status");
 
 let currentCity = "";
-let currentUnit = "f";
+let currentUnit = "c";
 let hourlyWeekly = "Week";
 
 //updating 
 
 function getDateTime() {
-    let now = new Date();
+    let now = new Date(),
     hour = now.getHours(),
     minute =now.getMinutes();
 
@@ -59,10 +59,11 @@ setInterval(() => {
 //function to go public
 
 function getPublicIp() {
-    fetch("https://geolocation-db.com/json/",
+    fetch("http://ip-api.com/json/",
     {
     method: "GET",
-    })
+    }
+)
     .then((response) => response.json())
         .then((data) => {
             console.log(data);
@@ -77,7 +78,7 @@ getPublicIp();
 
 function getWeatherData(city, unit, hourlyWeekly) {
     const apiKey = "N5T5LN5MK2QZN7LWV3WZPCR59";
-    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=${apiKey}&contentType=json`,
+    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=${apiKey}&contentType=json`, 
         {
         method: "GET",
         }
@@ -99,19 +100,52 @@ function getWeatherData(city, unit, hourlyWeekly) {
                 visibility.innerText = today.visibility;
                 airQuality.innerText = today.windir;
                 measureUvIndex(today.uvindex);
+                updateHumidityStatus(today.humidity);
+                updateVisibilityStatus(today.visibility);
+                updateAirQualityStatus(today.winddir);
+                sunRise.innerText = convertTimeTo12HourFormat(today.sunrise);
+                sunSet.innerText =  convertTimeTo12HourFormat(today.sunrise);
             });
 }
 
 //convert C to F
 function celsiusToFahrenheit(temp) {
-    return ((temp * 9) / 5 + 32).toFixed(1);
+}
+function measureUvIndex(uvIndex) {
+}
+function updateHumidityStatus(humidity) {
+}
+function updateVisibilityStatus(visibility) {
+}
+function updateAirQualityStatus(airQuality) {
+}
+function convertTimeTo12HourFormat(time) {
+    let hour = time.split(":")[0];
+    let minute = time.split(":")[1];
+
+        let ampm = hour >= 12 ? "pm" : "am";
+        hour = hour & 12;
+        hour = hour ? hour : 12; //0 hr ends up as 12
+        hour = hour < 10 ? "0" + hour : hour; 
+        minute = minute < 10 ? "0"+ minute : minute;
+
+        let startTime = hour + ":" + minute + ampm;
+        return startTime;
+}
+
+function cToF(celsius) 
+{
+  var cTemp = celsius;
+  var cToFahr = cTemp * 9 / 5 + 32;
+  var message = cTemp+'\xB0C is ' + cToFahr + ' \xB0F.';
+    console.log(message);
 }
 //function for uv index
 
 function measureUvIndex(uvIndex) {
     if(uvIndex <= 2) 
     {
-    uvText.innerText= "Low";
+    uvText.innerText = "Low";
     } else if (uvIndex <= 5) {
         uvText.innerText = "Moderate";
     } else if (uvIndex <= 7) {
@@ -120,5 +154,50 @@ function measureUvIndex(uvIndex) {
         uvText.innerText = "Very High";
     } else {
         uvText.innerText = "Extreme";
+    }
+}
+
+function updateHumidityStatus (humidity) {
+    if (humidity <= 30) {
+        humidityStatus.innerText = "Low";
+    } else if (humidity <= 60) {
+        humidityStatus.innerText = "Moderate";
+    } else {
+        humidityStatus.innerText = "High";
+    }
+}
+function updateVisibilityStatus(visibility) {
+    if (visibility <= 0.3) {
+        visibilityStatus.innerText = "Dense Fog";
+    }   else if (visibility <= 0.16) {
+        visibilityStatus.innerText = "Moderate Fog";
+    }   else if (visibility <= 0.35) {
+        visibilityStatus.innerText = "Light Fog";
+    }   else if (visibility <= 1.13) {
+        visibilityStatus.innerText = "Very Light Fog";
+    }   else if (visibility <= 2.16) {
+        visibilityStatus.innerText = "Light Mist";
+    }   else if (visibility <= 5.4) {
+        visibilityStatus.innerText = "Very Light Mist";
+    }   else if (visibility <= 10.8) {
+        visibilityStatus.innerText = "Clean Air";
+    }   else {
+        visibilityStatus.innerText = "Very Clean Air";
+    }
+}
+
+function updateAirQualityStatus(airQuality) {
+    if (airQuality <= 50) {
+        airQualityStatus.innerText = "Good";
+    } else if (airQuality <= 100) {
+        airQualityStatus.innerText = "Moderate";
+      } else if (airQuality <= 150) {
+        airQualityStatus.innerText = "Unhealthy for Sensitive Groups";
+      } else if (airQuality <= 200) {
+        airQualityStatus.innerText = "Unhealthy";
+      } else if (airQuality <= 250) {
+        airQualityStatus.innerText = "Very Unhealthy";
+      } else {
+        airQualityStatus.innerText = "Hazardous"; 
     }
 }
